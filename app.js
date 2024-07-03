6bd885d09e2f11b7cc39946ffffc20099f48672f
kiiiiiiiiif <kyle.vuanh@gmail.com>
2024-07-03 16:21:56 +0700

    [UPDATE] .env

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 var port = 443;
+const dotenv = require('dotenv');
+dotenv.config();
+
+const PORT = process.env.PORT;
+const MONGODB_KEY = process.env.MONGODB_KEY;
+const LOCAL_HOST = process.env.LOCAL_HOST;
 
 app.use(express.json());
 
@@ -8,8 +14,8 @@ app.get('/', (req, res) => {
     res.send("Deploying on render.com");
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.clear();
-    console.log("Starting server on port " + port);
-    console.log("http://localhost:" + port);
+    console.log(`Starting server on port ${PORT}`);
+    console.log(`${LOCAL_HOST}`);
 });
