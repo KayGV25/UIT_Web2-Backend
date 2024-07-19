319b8f253e0bcf507dd268d7b5d4a7d5657c25cb
KayGV <khuongvudang25@gmail.com>
2024-07-19 09:19:40 +0700

    [CHANGE] time in recipe from Number to String

--- models/recipe.js
+++ models/recipe.js
@@ -7,7 +7,7 @@ name: { type: String, required: true },
 ingredients: { type: String, required: true },
 instructions: { type: String, required: true },
 author: { type: String, required: true },
-time: { type: Number, required: true },
+time: { type: String, required: true },
 tags: { type: String, required: true },
 image: { type: String, required: true }
 }, { timestamps: true });
