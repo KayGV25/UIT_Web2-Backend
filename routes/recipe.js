5c5fe02e3ede2e1340c5ca3c44a51da6b8d615e4
KayGV <khuongvudang25@gmail.com>
2024-08-07 14:27:50 +0700

    [UPDATE] change upload url

--- routes/recipe.js
+++ routes/recipe.js
@@ -6,7 +6,7 @@ router.get("/recipes", recipeController.getAll);
 router.get("/recipes/:id", recipeController.getOne);
 router.get("/recipes/search", recipeController.getFilter);
 router.get("/recipes/reported", );
-router.post("/recipes", recipeController.createOne);
+router.post("/recipes/upload", recipeController.createOne);
 router.delete("/recipes/:id", )
 
 module.exports = router;
