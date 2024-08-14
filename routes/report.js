74d3542e4f9c0ed9562acc025c2f44b1399eb29f
kiiiiiiiiif <kyle.vuanh@gmail.com>
2024-08-14 13:58:41 +0700

    [UPDATE] reported routes

--- routes/report.js
+++ routes/report.js
@@ -4,7 +4,7 @@ const reportController = require("../controllers/reportControllers");
 const router = require("express").Router();
 
 router.get("/recipes/reports", authMiddleware.verifyAdmin, reportController.getAll);
-router.post("/recipes/reports/:id", authMiddleware.verifyUser, reportController.reportOne);
+router.post("/recipes/reports", authMiddleware.verifyUser, reportController.reportOne);
 router.delete("/recipes/reports/:id", authMiddleware.verifyAdmin, reportController.deleteOne);
 
 module.exports = router;
